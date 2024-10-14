9bf498c13a86dd173e2718814a567189457b26f6
Dan Popescu <pdan.popescu@gmail.com>
2024-10-14 09:22:00 +0300

    chore: only import package name and version from package.json

--- lib/src/libraryInfo.ts
+++ lib/src/libraryInfo.ts
@@ -1,12 +1,12 @@
 import * as paths from '$lib/paths.json';
 
-import packageJson from '../package.json' with { type: 'json'};
+import { name, version } from '../package.json' with { type: 'json'};
 
 /** Library name. */
-export const libName: string = packageJson.name;
+export const libName: string = name;
 
 /** Library version. */
-export const libVersion: string = packageJson.version;
+export const libVersion: string = version;
 
 /** Component names */
 export const libComponentNames: string[] = paths.components;
