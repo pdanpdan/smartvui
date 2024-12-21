4ffe9532eb6f89afa2683b10f40ea2afd99269c1
Dan Popescu <pdan.popescu@gmail.com>
2024-12-21 09:57:06 +0200

    chore: remove generic ts declaration for vue components

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -8,12 +8,12 @@ declare interface Window {
   // extend the window
 }
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
+// declare module '*.vue' {
+//   import type { DefineComponent } from 'vue';
 
-  const component: DefineComponent<object, object, unknown>;
-  export default component;
-}
+//   const component: DefineComponent<object, object, unknown>;
+//   export default component;
+// }
 
 declare module '*.svg' {
   const imageUrl: string;
