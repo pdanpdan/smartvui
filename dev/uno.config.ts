137f29cc420f02578a758165f4977f3b3fba42fb
Dan Popescu <pdan.popescu@gmail.com>
2025-03-07 03:21:15 +0200

    chore: update deps

--- dev/uno.config.ts
+++ dev/uno.config.ts
@@ -1,12 +1,12 @@
 import {
   defineConfig,
   presetIcons,
-  presetWind3,
+  presetWind4,
 } from 'unocss';
 
 export default defineConfig({
   presets: [
-    presetWind3({
+    presetWind4({
       attributifyPseudo: true,
     }),
     presetIcons({
